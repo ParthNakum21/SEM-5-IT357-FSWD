bdec4572dda1155675476c47f3268bf79c6cad70
Parth Nakum <parthnakum021@gmail.com>
2023-07-11 15:21:26 +0530

    Working with the readline modules

--- NodeJS/NodeJS_Tutorial/app.js
+++ NodeJS/NodeJS_Tutorial/app.js
@@ -29,8 +29,9 @@ class Person extends EventEmmiter{
     }
 }
 
-let elon = new Person('Elon'); // persom extends event emmiter class, instance emitter class
+let elon = new Person('Elon'); // person extends event emmiter class, instance emitter class
 let zuck = new Person('Zuck');
+let tata = new Person('Tata');
 
 elon.on('name',()=>{
     console.log("My name is " + elon.name);
@@ -40,5 +41,45 @@ zuck.on('name',() => {
     console.log("My name is " + zuck.name);
 })
 
+tata.on('name',()=>{
+    console.log("My name is "+ tata.name );
+});
+
 elon.emit('name');
 zuck.emit('name');
+tata.emit('name');
+
+
+//-------------------Working with the read line module----------------------
+
+const readline = require('readline'); // realine module   
+const rl = readline.createInterface({input : process.stdin, output : process.stdout});
+
+let num1 = Math.floor((Math.random()*10)+1);
+let num2 = Math.floor((Math.random()*10)+1);
+
+let answer = num1 + num2;
+
+rl.question(`What is ${num1} + ${num2}? \n`, 
+(userInput) => {
+    if(userInput.trim() == answer){
+        rl.close();
+    }
+    else{
+        rl.setPrompt('Incorrect response, please try again \n');
+        rl.prompt();
+        rl.on('line',(userInput)=>{
+            if(userInput.trim() == answer){
+                rl.close();
+            }
+            else{
+                rl.setPrompt(`Your answer of ${userInput} is incorrect, try again \n`);
+                rl.prompt();
+            }
+        })
+    }
+});
+
+rl.on('close',()=>{
+    console.log('Correct!!');
+});
