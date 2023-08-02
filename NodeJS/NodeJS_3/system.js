f888371bcec3babb4d9b1c137441a272a25bf890
Parth Nakum <parthnakum021@gmail.com>
2023-08-02 21:09:03 +0530

    Updated Task-3

--- NodeJS/NodeJS_3/system.js
+++ NodeJS/NodeJS_3/system.js
@@ -1,5 +1,6 @@
 //---------------------WEEK 3--------------------------------
 
+
 //---------------------Task 1-------------------------------
 /*
 OS Information:
@@ -33,21 +34,21 @@ Write a program that uses the os module to display the current user's username, 
 Create a function that utilizes the os module to display the total system memory, free memory, and the percentage of free memory available
 */
 
-const os = require('os');
+// const os = require('os');
 
-function memoryInfo(){
-    const totalMemory = os.totalmem();
-    const freeMemory = os.freemem();
-    const percentageFree = ((freeMemory/totalMemory)*100);
+// function memoryInfo(){
+//     const totalMemory = os.totalmem();
+//     const freeMemory = os.freemem();
+//     const percentageFree = ((freeMemory/totalMemory)*100);
 
-    return {totalMemory,freeMemory,percentageFree};
-}
+//     return {totalMemory,freeMemory,percentageFree};
+// }
 
-const {totalMemory, freeMemory, percentageFree} = memoryInfo();
+// const {totalMemory, freeMemory, percentageFree} = memoryInfo();
 
-console.log("Total System Memory: ",totalMemory, "bytes");
-console.log("Free Memory: ",freeMemory, "bytes");
-console.log("Percentage of Free Memory: ", (percentageFree).toFixed(2), "%");
+// console.log("Total System Memory: ",totalMemory, "bytes");
+// console.log("Free Memory: ",freeMemory, "bytes");
+// console.log("Percentage of Free Memory: ", (percentageFree).toFixed(2), "%");
 
 
 /*
@@ -63,3 +64,11 @@ console.log("Percentage of Free Memory: ", (percentageFree).toFixed(2), "%");
 //create your own custom module and import/export it to main module
 
  */
+
+const myModule = require('./myModule');
+
+const sum = myModule.addNumbers(21,21);
+const product = myModule.multiplyNumbers(21,21);
+
+console.log("Sum: ", sum);
+console.log("Product: ", product);
